8df8f3811cbf7a680cd78cb842a859c18cd76814
Vijayrajdev <vijayrajck@outlook.com>
2022-10-11 11:37:29 +0530

    Contributors page corrections

--- src/pages/Contributors.js
+++ src/pages/Contributors.js
@@ -3,8 +3,8 @@ import React from "react";
 const Contributors = () => {
   return (
     <div>
-      <div className="flex items-center justify-evenly my-28">
-        <div className="border-2 rounded-lg px-10 py-12 flex flex-col items-center gap-4 min-w-[600px] animate__animated animate__zoomIn">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-10 md:justify-evenly my-10 md:my-28">
+        <div className="border-2 rounded-lg px-5 py-6 md:px-10 md:py-12 flex flex-col items-center gap-4 max-w-[300px]  md:min-w-[600px] animate__animated animate__zoomIn">
           <a
             target={"_blank"}
             href="https://github.com/Jayashreedev"
@@ -33,13 +33,10 @@ const Contributors = () => {
             <span className="text-base text-blue-700 font-semibold">
               Contribution
             </span>
-            <p>
-              Designing,Data collection, application corrections and Feature
-              updates.
-            </p>
+            <p>Data collection, Application corrections and Feature updates.</p>
           </div>
         </div>
-        <div className="border-2 rounded-lg px-10 py-12 flex flex-col items-center gap-4  min-w-[600px] animate__animated animate__zoomIn">
+        <div className="border-2 rounded-lg px-10 py-12 flex flex-col items-center gap-4 max-w-[300px]  md:min-w-[600px] animate__animated animate__zoomIn">
           <a
             target={"_blank"}
             href="https://github.com/Vijayrajdev"
